61f86767edc5aebcb497cfa02f1b09387529ea64
DESKTOP-NH96BIF\Administrator <1603729255@qq.com>
2022-06-01 18:03:01 +0800

    2022.6.1

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+const app = createApp(App);
+import "./assets/style/global.less";
+import router from './router'
+import store from "@/store/index";
+app.use(store);
+app.use(router)
 createApp(App).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import * as path from 'path'
+import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  css: {
+    // css预处理器
+    preprocessorOptions: {
+      less: {
+        additionalData: `@import "${resolve(__dirname, 'src/assets/style/global.less')}";`,
+        javascriptEnabled: true
+      }
+    }
+  }
 })
